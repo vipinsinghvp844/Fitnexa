3e2867814fc3e2d6ab75212225ae033429c12dc4
mitesh-123 <mitesh93official@gmail.com>
2026-05-22 14:36:49 +0530

    fix: change exported function name to proxy

--- proxy.ts
+++ proxy.ts
@@ -14,7 +14,7 @@ export const config = {
   ],
 };
 
-export function middleware(req: NextRequest) {
+export default function proxy(req: NextRequest) {
   const url = req.nextUrl;
 
   // Determine if this is a subdomain or a custom domain
